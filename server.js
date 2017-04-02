497953c6e44db89a0dfc1c3ad2f12afb5b3c0b98
Jeffrey Horn <jeffh@generalassemb.ly>
2017-04-02 16:28:52 -0400

    Make standard and address warnings
    Some specs still failing due to change from mongoose to bookshelf

--- server.js
+++ server.js
@@ -1,57 +1,57 @@
-'use strict';
+'use strict'
 require('dotenv').load({
-  silent: process.env.NODE_ENV === 'production', // don't log missing .env
-});
+  silent: process.env.NODE_ENV === 'production' // don't log missing .env
+})
 
-const express = require('express');
-const app = express();
-const middleware = require('app/middleware');
+const express = require('express')
+const app = express()
+const middleware = require('app/middleware')
 
-app.set('root', __dirname);
+app.set('root', __dirname)
 
-middleware.before(app);
+middleware.before(app)
 
-const routes = require('config/routes');
+const routes = require('config/routes')
 
-app.use(routes.router);
+app.use(routes.router)
 
-middleware.after(app);
+middleware.after(app)
 
 // catch 404 and forward to error handler
-app.use(middleware['404']);
+app.use(middleware['404'])
 
 // error handlers
-app.use(middleware['error-handler']);
+app.use(middleware['error-handler'])
 
-const debug = require('debug')('schism-api:server');
-const http = require('http');
+const debug = require('debug')('schism-api:server')
+const http = require('http')
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
 const normalizePort = (val) => {
-  let port = parseInt(val, 10);
-  debug('Normalied port is', port);
-  return port >= 0 ? port : isNaN(port) ? val : false;
-};
+  const port = parseInt(val, 10)
+  debug('Normalied port is', port)
+  return port >= 0 ? port : isNaN(port) ? val : false
+}
 
 /**
  * Get port from environment and store in Express.
  */
 
-let devPort = +('GH'.split('').reduce((p, c) =>
+const devPort = +('GH'.split('').reduce((p, c) =>
  p + c.charCodeAt().toString(16), '')
-);
+)
 
-const port = normalizePort(process.env.PORT || devPort);
-app.set('port', port);
+const port = normalizePort(process.env.PORT || devPort)
+app.set('port', port)
 
 /**
  * Create HTTP server.
  */
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 /**
  * Event listener for HTTP server "error" event.
@@ -59,46 +59,46 @@ const server = http.createServer(app);
 
 const onError = (error) => {
   if (error.syscall !== 'listen') {
-    throw error;
+    throw error
   }
 
-  const bind = typeof port === 'string' ?
-    'Pipe ' + port :
-    'Port ' + port;
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
+      console.error(bind + ' is already in use')
+      process.exit(1)
+      break
     default:
-      throw error;
+      throw error
   }
-};
+}
 
 /**
  * Event listener for HTTP server "listening" event.
  */
 
-const onListening = () =>  {
-  const addr = server.address();
-  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-  console.log('Server listening on ' + bind);
-};
+const onListening = () => {
+  const addr = server.address()
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+  console.log('Server listening on ' + bind)
+}
 
 /**
  * Listen on provided port, on all network interfaces.
  */
 
-server.on('error', onError);
-server.on('listening', onListening);
-server.listen(port);
+server.on('error', onError)
+server.on('listening', onListening)
+server.listen(port)
 
 module.exports = {
-  server,
-};
+  server
+}
--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,60 +1,60 @@
-'use strict';
+'use strict'
 
-const request = require('supertest');
-const User = require('../app/models/user');
+const request = require('supertest')
+// const User = require('../app/models/user')
 
 describe('loading express', function () {
-  let server;
+  let server
 
   beforeEach(function () {
-    let app = require('../server');
-    server = app.server;
+    const app = require('../server')
+    server = app.server
 
     // remove any users that exist
-    User.remove(err => err && console.error(err));
-  });
+    // User.remove(err => err && console.error(err))
+  })
 
   afterEach(function () {
-    server.close();
+    server.close()
 
     // remove any users that exist
-    User.remove(err => err && console.error(err));
-  });
+    // User.remove(err => err && console.error(err))
+  })
 
-  it('responds to /', function testSlash(done) {
+  it('responds to /', function testSlash (done) {
     request(server)
       .get('/')
-      .expect(200, done);
-  });
+      .expect(200, done)
+  })
 
-  it('gets 404 for unknown routes', function testPath(done) {
+  it('gets 404 for unknown routes', function testPath (done) {
     request(server)
       .get('/foo/bar')
-      .expect(404, done);
-  });
+      .expect(404, done)
+  })
 
-  it("get '/examples' returns json", function getExample(done) {
+  it("get '/examples' returns json", function getExample (done) {
     request(server)
       .get('/examples')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200, done);
-  });
+      .expect(200, done)
+  })
 
-  it("post '/sign-up' creates a user", function postUser(done) {
+  it("post '/sign-up' creates a user", function postUser (done) {
     const data = {
       credentials: {
         email: 'r@r.com',
         password: 'izcool',
-        password_confirmation: 'izcool',
-      },
-    };
+        password_confirmation: 'izcool'
+      }
+    }
     request(server)
       .post('/sign-up')
       .set('Content-Type', 'application/json')
       .set('Accept', 'application/json')
       .send(data)
       .expect('Content-Type', /json/)
-      .expect(201, done);
-  });
-});
+      .expect(201, done)
+  })
+})
